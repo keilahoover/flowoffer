7bd3a49a4f88126e4911efacf78bc8ce30567999
Keila Hoover <keila.hoover@gmail.com>
2018-04-02 14:41:58 -0600

    delete duplicate flowoffer and add offer route

---
+++ routes/offer.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET all offers. */
+router.get('/', function(req, res, next) {
+  res.send('GET all offers');
+});
+
+// GET a single offer
+router.get('/:id', function (req, res, next) {
+  res.send('GET a single offer')
+})
+
+//POST a new offer
+router.post('/', function (req, res, next) {
+  res.send('POST a new offer')
+})
+
+// DELETE an offer
+router.delete('/:id', function (req, res, next) {
+  res.send('DELETE an offer')
+})
+
+// UPDATE an offer
+router.patch('/:id', function (req, res, next) {
+  res.send('UPDATE an offer')
+})
+
+module.exports = router;
