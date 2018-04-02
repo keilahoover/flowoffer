fa046b780e9d85819e8fd5d9181b5854aaedc5cc
Keila Hoover <keila.hoover@gmail.com>
2018-04-02 16:20:56 -0600

    Add Procfile and update package.json to prepare to deploy on heroku

--- routes/offer.js
+++ routes/offer.js
@@ -15,22 +15,46 @@ router.get('/:id', function (req, res, next) {
   knex('offer')
     .select('id', 'name')
     .where('id', id)
-    .then(rows => res.json(rows))
+    .then(rows => {
+      if (rows.length > 0) {
+        res.json(rows)
+      } else {
+        res.sendStatus(404)
+      }
+    })
 })
 
-//POST a new offer
+// POST a new offer
 router.post('/', function (req, res, next) {
-  res.send('POST a new offer')
+  const { name } = req.body
+
+  knex('offer')
+    .insert([
+      { name: name }
+    ])
+    .returning('id')
+    .then(result => res.json(result))
 })
 
 // DELETE an offer
 router.delete('/:id', function (req, res, next) {
-  res.send('DELETE an offer')
+  const { id } = req.params
+
+  knex('offer')
+  .del('id', id)
+  .then(result => res.json(result))
 })
 
 // UPDATE an offer
 router.patch('/:id', function (req, res, next) {
-  res.send('UPDATE an offer')
+  const { id } = req.params
+  const { name } = req.body
+
+  knex('offer')
+    .update({ name: name })
+    .where('id', id)
+    .returning('id')
+    .then(result => res.json(result))
 })
 
 module.exports = router;
